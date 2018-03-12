4e56cf0b92aab498e2205ab70c4da72022e29a68
Kostia Talamaniuk <kostia.talamaniuk@gmail.com>
2018-03-12 16:18:42 +0200

    initial

--- JavaTutorial/src/Introduction/Second.java
+++ JavaTutorial/src/Introduction/Second.java
@@ -1,8 +1,12 @@
+import java.util.LinkedList;
+
 public class Second {
     public static void main(String[] args) {
         System.out.println("Second");
         Third t = new Third();
         t.toString();
         First f = new First();
+
+        LinkedList test = new LinkedList();
     }
 }
---
+++ JavaTutorial/src/sandbox/OddDigits.java
@@ -0,0 +1,15 @@
+package sandbox;
+
+import java.util.Random;
+
+public class OddDigits {
+    public static void main(String[] args) {
+        Random r = new Random();
+        for (int a = 1; a <= 10; a++) {
+            int value = r.nextInt(12); // generate random int from 0 to 11 and put to the value
+            int result = value % 2; // divide value by 2 and put to the result
+            if (result == 1) // check if the value has the remainder
+                System.out.println(value + " "); //print into console all the even digits
+        }
+    }
+}
